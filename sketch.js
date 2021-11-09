ca0146349a584e2c99ca6c50cc58a301ae0234af
kev-kev <kevin.clark93@gmail.com>
2021-11-09 14:24:43 -0500

    click handler for boxes

--- sketch.js
+++ sketch.js
@@ -7,26 +7,18 @@ function setup() {
 
 function draw() {
   drawGrid();
-  createBoxes();
 }
 
 const handleBoxSelect = () => {
   let x = Math.floor(mouseX / BOX_SIZE);
   let y = Math.floor(mouseY / BOX_SIZE);
-  console.log(x, y);
+  fill("blue");
+  rect(x * BOX_SIZE, y * BOX_SIZE, BOX_SIZE, BOX_SIZE);
 };
 
 const drawGrid = () => {
   for (let i = 0; i <= height; i += BOX_SIZE) {
     line(i, 0, i, height);
     line(0, i, width, i);
-  }
-};
-
-const createBoxes = () => {
-  for (let i = 0; i < height; i += BOX_SIZE) {
-    for (let j = 0; j < width; j += BOX_SIZE) {
-      rect(i, j, BOX_SIZE, BOX_SIZE);
-    }
   }
 };
